d538dbcaaeb43363c83d9072fdaa541e42a0bba9
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-02-08 18:49:24 +0000

    feature: create socket and class Thread

--- src/com/yogusoft/Main.java
+++ src/com/yogusoft/Main.java
@@ -1,6 +1,39 @@
 package com.yogusoft;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
+        Socket socket;
+
+        try (ServerSocket serverSockets = new ServerSocket(8080)) {
+            System.out.println("Starting Chat Server");
+
+            while (true) {
+                socket = serverSockets.accept();
+                Chat newChat = new Chat(socket);
+
+                newChat.start();
+            }
+
+        } catch (IOException e) {
+            String messageErr = e.getMessage();
+
+            if (messageErr == null) {
+                e.printStackTrace();
+            } else {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private static class Chat extends Thread {
+        private Socket socket;
+
+        public Chat(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
